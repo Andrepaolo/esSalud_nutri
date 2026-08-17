90865ce2e9c1ceaac603206c6fbcece00dae0d27
Andrepaolo <andrepaol32@gmail.com>
2026-08-17 09:38:20 -0500

    Forzar HTTPS detras de Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +19,11 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {   
-        
+    {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         config(['app.timezone' => 'America/Lima']);
         date_default_timezone_set('America/Lima');
     }
